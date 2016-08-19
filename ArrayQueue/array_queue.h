b1eb757db916a627eed5062e06334af6a5d616df
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-08-18 23:56:26 -0300

    Now implementations of the methods are in the header file
    Using this because template classes need to be defined inside its header files.

--- ArrayQueue/array_queue.h
+++ ArrayQueue/array_queue.h
@@ -39,6 +39,93 @@ private:
     const static auto DEFAULT_SIZE = 10u;
 };
 
+template<typename T>
+ArrayQueue<T>::ArrayQueue()
+{
+    contents = new T[DEFAULT_SIZE];
+    size_ = 0;
+    max_size_ = DEFAULT_SIZE;
+}
+
+template<typename T>
+ArrayQueue<T>::ArrayQueue(std::size_t max)
+{
+    contents = new T[max];
+    size_ = 0;
+    max_size_ = max;
+}
+
+template<typename T>
+ArrayQueue<T>::~ArrayQueue()
+{
+    delete[] contents;
+}
+
+template<typename T>
+void ArrayQueue<T>::enqueue(const T& data)
+{
+    if (full()) {
+        throw std::out_of_range("Queue is full");
+    } else {
+        contents[size_++] = data;
+    }
+}
+
+template<typename T>
+T ArrayQueue<T>::dequeue()
+{
+    if (empty()) {
+        throw std::out_of_range("Queue is empty");
+    } else {
+        T first = contents[0];
+        for (int i = 1; i < size(); ++i) {
+            contents[i-1] = contents[i];
+        }
+        size_--;
+        return first;
+    }
+}
+
+template<typename T>
+T& ArrayQueue<T>::back()
+{
+    if (empty()) {
+        throw std::out_of_range("Queue is empty");
+    } else {
+        return contents[size_-1];
+    }
+}
+
+template<typename T>
+void ArrayQueue<T>::clear()
+{
+    size_ = 0;
+}
+
+template<typename T>
+std::size_t ArrayQueue<T>::size()
+{
+    return size_;
+}
+
+template<typename T>
+std::size_t ArrayQueue<T>::max_size()
+{
+    return max_size_;
+}
+
+template<typename T>
+bool ArrayQueue<T>::empty()
+{
+    return size_ == 0;
+}
+
+template<typename T>
+bool ArrayQueue<T>::full()
+{
+    return size_ == max_size_;
+}
+
 }
 
 #endif
--- ArrayStack/array_stack.h
+++ ArrayStack/array_stack.h
@@ -39,6 +39,88 @@ private:
     const static auto DEFAULT_SIZE = 10u;
 };
 
+template<typename T>
+ArrayStack<T>::ArrayStack()
+{
+    contents = new T[DEFAULT_SIZE];
+    top_ = -1;
+    max_size_ = DEFAULT_SIZE;
+}
+
+template<typename T>
+ArrayStack<T>::ArrayStack(std::size_t max)
+{
+    contents = new T[max];
+    top_ = -1;
+    max_size_ = max;
+}
+
+template<typename T>
+ArrayStack<T>::~ArrayStack()
+{
+    delete[] contents;
+}
+
+template<typename T>
+void ArrayStack<T>::push(const T& data)
+{
+    if (full()) {
+        throw std::out_of_range("Stack is full");
+    } else {
+        contents[++top_] = data;
+    }
+}
+
+template<typename T>
+T ArrayStack<T>::pop()
+{
+    if (empty()) {
+        throw std::out_of_range("Stack is empty");
+    } else {
+        return contents[top_--];
+    }
+}
+
+template<typename T>
+T& ArrayStack<T>::top()
+{
+    if (empty()) {
+        throw std::out_of_range("Stack is empty");
+    } else {
+        return contents[top_];
+    }
+}
+
+template<typename T>
+void ArrayStack<T>::clear()
+{
+    top_ = -1;
+}
+
+template<typename T>
+std::size_t ArrayStack<T>::size()
+{
+    return top_ + 1;
+}
+
+template<typename T>
+std::size_t ArrayStack<T>::max_size()
+{
+    return max_size_;
+}
+
+template<typename T>
+bool ArrayStack<T>::empty()
+{
+    return top_ == -1;
+}
+
+template<typename T>
+bool ArrayStack<T>::full()
+{
+    return top_ == max_size_ - 1;
+}
+
 }
 
 #endif
