d92a0a96538595a7bc178b0245fa97270b67239c
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-09-14 21:41:11 -0300

    Documentation added

--- linked-structures/linked_queue.h
+++ linked-structures/linked_queue.h
@@ -8,21 +8,43 @@
 
 namespace structures {
 
+/**
+ * @brief Implements a Queue(data structure).
+ * @details This data structure works like a real 'queue', you add(enqueue)
+ * elements to the end(tail) of the queue, and remove(dequeue) from the
+ * beginning(head). It follows the FIFO(first in, first out) principle.
+ *
+ * @tparam T This is the data type of the elements of the queue.
+ */
 template<typename T>
 class LinkedQueue {
-	public:
+public:
+	/**
+	 * @brief Default constructor
+	 */
 	LinkedQueue() = default;
 
+	/**
+	 * @brief Destructor
+	 */
 	~LinkedQueue() {
 		clear();
 	}
 
+	/**
+	 * @brief Clears the queue
+	 */
 	void clear() {
 		while (!empty()) {
 			dequeue();
 		}
 	}
 
+	/**
+	 * @brief Adds 'data' to the end of the queue
+	 *
+	 * @param data The element that'll be added to the queue
+	 */
 	void enqueue(const T& data) {
 		auto newNode = new Node(data);
 		if (empty()) {
@@ -35,6 +57,13 @@ class LinkedQueue {
 		++size_;
 	}
 
+	/**
+	 * @brief Removes the element at the beginning of the queue
+	 * @details Checks if the queue is not empty, then returns and removes the first
+	 * element of the queue. Throws std::out_of_range if the queue is empty
+	 *
+	 * @return The element that was removed
+	 */
 	T dequeue() {
 		if (empty())
 			throw std::out_of_range("Queue is empty");
@@ -47,29 +76,51 @@ class LinkedQueue {
 		return out;
 	}
 
+	/**
+	 * @brief Returns the first element of the queue
+	 * @details Checks if the queue is not empty, then returns the first element
+	 * on it. Throws std::out_of_range if the queue is empty.
+	 *
+	 * @return A reference to the first element.
+	 */
 	T& front() const {
 		if (empty())
 			throw std::out_of_range("Queue is empty");
 		return head->data();
 	}
 
+	/**
+	 * @brief Returns the last element of the queue
+	 * @details Checks if the queue is not empty, then returns the last element
+	 * on it. Throws std::out_of_range if the queue is empty.
+	 *
+	 * @return A reference to the last element.
+	 */
 	T& back() const {
 		if (empty())
 			throw std::out_of_range("Queue is empty");
 		return tail->data();
 	}
 
+	/**
+	 * @brief Checks if the queue is empty
+	 *
+	 * @return True if queue is empty
+	 */
 	bool empty() const {
 		return size_ == 0;
 	}
 
+	/**
+	 * @brief Returns the number of elements on the queue.
+	 */
 	std::size_t size() const {
 		return size_;
 	}
 
-	private:
+private:
 	class Node {
-		public:
+	public:
 		explicit Node(const T& data): data_{data} {}
 		Node(const T& data, Node* next): data_{data}, next_{next} {}
 
@@ -93,7 +144,7 @@ class LinkedQueue {
 			next_ = next;
 		}
 
-		private:
+	private:
 		T data_;
 		Node* next_{nullptr};
 	};
--- linked-structures/linked_stack.h
+++ linked-structures/linked_stack.h
@@ -8,26 +8,55 @@
 
 namespace structures {
 
+/**
+ * @brief Implements a stack(data structure).
+ * @details This data structure works just like a real 'stack', you add elements
+ * to the top of the stack (push), and you remove from the top (pop).
+ * It follows the 'LIFO' (last in, first out) principle.
+ *
+ * @tparam T This is the data type of the elements of the stack.
+ */
 template<typename T>
 class LinkedStack {
-	public:
+public:
+	/**
+	 * @brief Default constructor
+	 */
 	LinkedStack() = default;
 
+	/**
+	 * @brief Destructor
+	 */
 	~LinkedStack() {
 		clear();
 	}
 
+	/**
+	 * @brief Clears the stack
+	 * @details Pops all elements of the list
+	 */
 	void clear() {
 		while (!empty()) {
 			pop();
 		}
 	}
 
+	/**
+	 * @brief Adds 'data' to the top of the stack
+	 * @param data The element that'll be added to the stack
+	 */
 	void push(const T& data) {
 		top_ = new Node(data, top_);
 		++size_;
 	}
 
+	/**
+	 * @brief Removes the element at the top of the stack
+	 * @details Checks if the stack is not empty, then returns and removes the
+	 * top element of the stack. Throws std::out_of_range if the stack is empty
+	 *
+	 * @return The element that was removed
+	 */
 	T pop() {
 		if (empty())
 			throw std::out_of_range("Stack is empty");
@@ -40,23 +69,37 @@ class LinkedStack {
 		return out;
 	}
 
+	/**
+	 * @brief Returns the top element of the stack
+	 * @details Checks if stack is not empty, then returns the element at the
+	 * top of the stack. Throws std::out_of_range if the stack is empty
+	 *
+	 * @return A reference to the top element of the stack
+	 */
 	T& top() const {
 		if (empty())
 			throw std::out_of_range("Stack is empty");
 		return top_->data();
 	}
 
+	/**
+	 * @brief Checks if stack is empty
+	 * @return True if stack is empty
+	 */
 	bool empty() const {
 		return size_ == 0;
 	}
 
+	/**
+	 * @brief Returns the number of elements on the stack
+	 */
 	std::size_t size() const {
 		return size_;
 	}
 
-	private:
+private:
 	class Node {
-		public:
+	public:
 		explicit Node(const T& data): data_{data} {}
 		Node(const T& data, Node* next): data_{data}, next_{next} {}
 
@@ -80,7 +123,7 @@ class LinkedStack {
 			next_ = next;
 		}
 
-		private:
+	private:
 		T data_;
 		Node* next_{nullptr};
 	};
