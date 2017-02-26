bc4c19a650943f555839948b9e8f0cfaeae38028
Matheus Bittencourt <matheuspb2005@hotmail.com>
2017-02-25 22:59:31 -0300

    Refactored circular list

--- linked-structures/circular_list.h
+++ linked-structures/circular_list.h
@@ -7,199 +7,199 @@
 namespace structures {
 
 /**
- * @brief Implements a circular linked list
- * @details This is an implementation of a circular linked list. Here, head
- * always points to a 'sentinel' node, so the list is never empty, and the last
- * node always points to 'head' (sentinel).
- *
- * @tparam T Data type of the elements
- */
+@brief Implements a circular linked list
+@details This is an implementation of a circular linked list. Here, head
+always points to a 'sentinel' node, so the list is never empty, and the last
+node always points to 'head' (sentinel).
+
+@tparam T Data type of the elements
+*/
 template<typename T>
 class CircularList {
 public:
 	/**
-	 * @brief Default constructor
-	 */
+	@brief Default constructor
+	*/
 	CircularList() {
-		auto sentinel = new Node(0);
-		sentinel->next(sentinel);
+		auto sentinel = new Node();
+		sentinel->next = sentinel;
 		head = sentinel;
 	}
 
 	/**
-	 * @brief Destructor
-	 */
+	@brief Destructor
+	*/
 	~CircularList() {
 		clear();
 		delete head;
 	}
 
 	/**
-	 * @brief Clears the list
-	 */
+	@brief Clears the list
+	*/
 	void clear() {
 		while(!empty())
 			pop_front();
 	}
 
 	/**
-	 * @brief Inserts at the end of the list
-	 *
-	 * @param data The element that'll be inserted
-	 */
+	@brief Inserts at the end of the list
+
+	@param data The element that'll be inserted
+	*/
 	void push_back(const T& data) {
 		insert(data, size_);
 	}
 
 	/**
-	 * @brief Inserts at the beginning of the list
-	 *
-	 * @param data The element that'll be inserted
-	 */
+	@brief Inserts at the beginning of the list
+
+	@param data The element that'll be inserted
+	*/
 	void push_front(const T& data) {
 		insert(data, 0);
 	}
 
 	/**
-	 * @brief Inserts at a given position of the list
-	 *
-	 * @param data The element that'll be inserted
-	 * @param index The position where 'data' will be inserted
-	 */
+	@brief Inserts at a given position of the list
+
+	@param data The element that'll be inserted
+	@param index The position where 'data' will be inserted
+	*/
 	void insert(const T& data, std::size_t index) {
 		if (index > size_) throw std::out_of_range("Invalid index");
 		Node* it = head;
 		for (std::size_t i = 0; i < index; ++i) {
-			it = it->next();
+			it = it->next;
 		}
-		it->next(new Node(data, it->next()));
+		it->next = new Node(data, it->next);
 		++size_;
 	}
 
 	/**
-	 * @brief Inserts the element sorted into the list
-	 *
-	 * @param data The element that'll be inserted
-	 */
+	@brief Inserts the element sorted into the list
+
+	@param data The element that'll be inserted
+	*/
 	void insert_sorted(const T& data) {
-		if (empty() || data <= head->next()->data())
+		if (empty() || data <= head->next->data)
 			return insert(data, 0);
-		Node* it = head->next();
-		while (it->next() != head && data > it->next()->data()) {
-			it = it->next();
+		Node* it = head->next;
+		while (it->next != head && data > it->next->data) {
+			it = it->next;
 		}
-		it->next(new Node(data, it->next()));
+		it->next = new Node(data, it->next);
 		++size_;
 	}
 
 	/**
-	 * @brief Checks if the index is valid, then returns a reference to the
-	 * element at the given index of the list
-	 *
-	 * @param index The index on the list of the element that'll be returned
-	 *
-	 * @return A reference to the element at the given index
-	 */
+	@brief Checks if the index is valid, then returns a reference to the
+	element at the given index of the list
+
+	@param index The index on the list of the element that'll be returned
+
+	@return A reference to the element at the given index
+	*/
 	T& at(std::size_t index) {
 		if (index >= size_) throw std::out_of_range("Index out of bounds");
-		Node* it = head->next();
+		Node* it = head->next;
 		for (std::size_t i = 0; i < index; ++i) {
-			it = it->next();
+			it = it->next;
 		}
-		return it->data();
+		return it->data;
 	}
 
 	/**
-	 * @brief Removes the element at the given index
-	 *
-	 * @param index The index of the element that'll be removed
-	 *
-	 * @return The element that was removed
-	 */
+	@brief Removes the element at the given index
+
+	@param index The index of the element that'll be removed
+
+	@return The element that was removed
+	*/
 	T pop(std::size_t index) {
 		if (index >= size_) throw std::out_of_range("Index out of bounds");
 		Node* it = head;
 		for (std::size_t i = 0; i < index; ++i) {
-			it = it->next();
+			it = it->next;
 		}
-		T removed = it->next()->data();
-		Node* p_removed = it->next();
-		it->next(it->next()->next());
+		T removed = it->next->data;
+		Node* p_removed = it->next;
+		it->next = it->next->next;
 		--size_;
 		delete p_removed;
 		return removed;
 	}
 
 	/**
-	 * @brief Removes the element at the end of the list
-	 *
-	 * @return The removed element
-	 */
+	@brief Removes the element at the end of the list
+
+	@return The removed element
+	*/
 	T pop_back() {
 		return pop(size_ - 1);
 	}
 
 	/**
-	 * @brief Removes the element at the beginning of the list
-	 *
-	 * @return The removed element
-	 */
+	@brief Removes the element at the beginning of the list
+
+	@return The removed element
+	*/
 	T pop_front() {
 		return pop(0);
 	}
 
 	/**
-	 * @brief Removes 'data' from the list
-	 *
-	 * @param data The element that'll be removed
-	 */
+	@brief Removes 'data' from the list
+
+	@param data The element that'll be removed
+	*/
 	void remove(const T& data) {
 		auto it = head;
-		for (; it->next()->data() != data; it = it->next()) {
-			if (it->next() == head)
+		for (; it->next->data != data; it = it->next) {
+			if (it->next == head)
 				return;
 		}
-		Node* p_removed = it->next();
-		it->next(it->next()->next());
+		Node* p_removed = it->next;
+		it->next = it->next->next;
 		delete p_removed;
 		--size_;
 	}
 
 	/**
-	 * @brief Checks if the list is empty
-	 *
-	 * @return True if the list is empty
-	 */
+	@brief Checks if the list is empty
+
+	@return True if the list is empty
+	*/
 	bool empty() const {
 		return size_ == 0;
 	}
 
 	/**
-	 * @brief Checks if the list contains an element(data)
-	 *
-	 * @param data The element that'll be checked if it is contained by the list
-	 *
-	 * @return True if the list contains 'data'
-	 */
+	@brief Checks if the list contains an element(data)
+
+	@param data The element that'll be checked if it is contained by the list
+
+	@return True if the list contains 'data'
+	*/
 	bool contains(const T& data) const {
-		for (auto it = head->next(); it != head; it = it->next()) {
-			if (it->data() == data)
+		for (auto it = head->next; it != head; it = it->next) {
+			if (it->data == data)
 				return true;
 		}
 		return false;
 	}
 
 	/**
-	 * @brief Returns the position of 'data' on the list
-	 *
-	 * @param data The element that'll be searched
-	 *
-	 * @return The index of 'data' on the list
-	 */
+	@brief Returns the position of 'data' on the list
+
+	@param data The element that'll be searched
+
+	@return The index of 'data' on the list
+	*/
 	std::size_t find(const T& data) const {
 		std::size_t index = 0;
-		for (auto it = head->next(); it != head; it = it->next()) {
-			if (it->data() == data)
+		for (auto it = head->next; it != head; it = it->next) {
+			if (it->data == data)
 				break;
 			++index;
 		}
@@ -207,36 +207,24 @@ public:
 	}
 
 	/**
-	 * @brief Size of the list
-	 *
-	 * @return Size of the list
-	 */
+	@brief Size of the list
+
+	@return Size of the list
+	*/
 	std::size_t size() const {
 		return size_;
 	}
 
 private:
-	class Node {
-	public:
-		explicit Node(const T& data): data_{data} {}
+	struct Node {
 
-		Node(const T& data, Node* next): data_{data}, next_{next} {}
+		Node() = default;
+		explicit Node(const T& data): data{data} {}
+		Node(const T& data, Node* next): data{data}, next{next} {}
 
-		T& data() {
-			return data_;
-		}
+		T data{};
+		Node* next{nullptr};
 
-		Node* next() const {
-			return next_;
-		}
-
-		void next(Node* node) {
-			next_ = node;
-		}
-
-	private:
-		T data_;
-		Node* next_{nullptr};
 	};
 
 	Node* head;
